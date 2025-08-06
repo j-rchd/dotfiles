0a8570ebf818a7716d21e151fae6c86403e7097c
j-rchd <joel.a.richards12@gmail.com>
2025-08-06 18:12:05 +0100

    Various changes to the debugger

--- .config/nvim/lua/plugins/debugging.lua
+++ .config/nvim/lua/plugins/debugging.lua
@@ -1,18 +1,16 @@
 return {
-	"mfussenegger/nvim-dap",
+	{"mfussenegger/nvim-dap",
 	dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio", "mfussenegger/nvim-dap-python" },
 	config = function()
 		local dap = require("dap")
 		local dapui = require("dapui")
 		local pydebug = require("dap-python")
-		local venv_path = os.getenv("PYENV_VERSION")
+		local venv_path = os.getenv("VIRTUAL_ENV")
 		if venv_path then
-			local py_path = os.getenv("HOME") .. "/.pyenv/versions/"
-			pydebug.setup(py_path .. venv_path .. "/bin/python3")
+			pydebug.setup(venv_path .."/bin/python3")
 		else
 			pydebug.setup()
 		end
-
 		dapui.setup({})
 
 		dap.listeners.before.attach.dapui_config = function()
@@ -29,5 +27,12 @@ return {
 		end
 		vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
 		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+		vim.keymap.set("n", "<Leader>dr", dap.restart, {})
+		vim.keymap.set("n", "<Leader>ds", dap.close, {})
+		vim.keymap.set("n", "<Leader>dd", dap.disconnect, {})
+		vim.keymap.set("n", "<Leader>duc", dapui.close, {})
+
 	end,
+	},
 }
+
